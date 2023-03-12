a4fcf62e5d0416de37e7c6cd757ad9aa07444fb2
Sebastian-Iratcabal <iratcabal@gmail.com>
2023-03-12 15:03:28 -0300

    seccion Boxes con tab de imagen y contenido de caja funcionando.

---
+++ js/cosas.js
@@ -0,0 +1,18 @@
+function openCity(cityName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+  
+    // Show the specific tab content
+    document.getElementById(cityName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
